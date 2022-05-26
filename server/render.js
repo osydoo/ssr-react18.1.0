c93970b100259184ba18cb46177e4319eee60af6
ydw0628 <ydw0628@midasin.co>
2022-05-26 16:42:23 +0900

    - code 가져오기 완료 - 18.0.0에서 18.1.0으로 변경되면서 변한 사항들 수정

---
+++ server/render.js
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { renderToPipeableStream } from 'react-dom/server'; // 이름이 변경되었다.
+import App from '../src/App';
+import {DataProvider} from '../src/data';
+import {API_DELAY, ABORT_DELAY} from './delays';
+
+let assets = {
+    'main.js': '/main.js',
+    'main.css': '/main.css'
+}
+
+module.exports = function render(url, res){
+    res.socket.on('error', error => {
+        console.error('Fatal', error);
+    });
+    let didError = false;
+    const data = createServerData();
+    const {pipe, abort} = renderToPipeableStream(
+        <DataProvider data={data}>
+            <App assets={assets} />
+        </DataProvider>,
+        {
+            onReadyToStream(){
+                res.statusCode = didError ? 500 : 200;
+                res.setHeader('Content-type', 'text/html');
+                res.write('<!DOCTYPE html>');
+                pipe(res);
+            },
+            onError(x){
+                didError = true;
+                console.error(x);
+            }
+        }
+    );
+    setTimeout(abort, ABORT_DELAY);
+}
+
+function createServerData(){
+    let done = false;
+    let promise = null;
+    return {
+        read() {
+            if(done){
+                return ;
+            }
+            if(promise){
+                throw promise;
+            }
+            promise = new Promise(resolve => {
+                setTimeout(()=>{
+                    done = true;
+                    promise = null;
+                    resolve();
+                }, API_DELAY);
+            });
+            throw promise;
+        }
+    }
+}
---
+++ src/Html.js
@@ -0,0 +1,18 @@
+export default function Html({assets, children, title}){
+    return (
+        <html lang={"en"}>
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content={"width=device-width, initial-scale=1"}/>
+                <link rel={"shortcut icon"} href={"favicion.ico"}/>
+                <link rel={"stylesheet"} href={assets['main.css']} />
+                <title>{title}</title>
+            </head>
+            <body>
+                <noscript dangerouslySetInnerHTML={{__html: `<b>Enable JavaScript to run this app.</b>`}}/>
+                {children}
+            <script dangerouslySetInnerHTML={{__html: `assetManifest = ${JSON.stringify(assets)};`}}/>
+            </body>
+        </html>
+    )
+}
